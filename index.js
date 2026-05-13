1c793d8f2a2aa199f8c9648bf9288bdd817d5acf
deedeetype <admin@wyzenews.com>
2026-05-13 00:21:06 -0400

    Architecture: Decoupled BrowserView from HTML UI definitively

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, BrowserView, ipcMain } = require('electron');
 const path = require('path');
 
+let mainWindow;
+let view;
+
 function createWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     frame: false,
@@ -11,11 +14,28 @@ function createWindow() {
       nodeIntegration: false,
       contextIsolation: true,
       preload: path.join(__dirname, 'preload.js'),
-      webviewTag: true
     },
   });
 
   mainWindow.loadFile('index.html');
+
+  // CREATION DU MOTEUR EN TANT QUE VUE INDEPENDANTE
+  view = new BrowserView();
+  mainWindow.setBrowserView(view);
+  
+  // POSITIONNEMENT STRICT : On laisse 250px pour la sidebar
+  view.setBounds({ x: 250, y: 0, width: 950, height: 800 });
+  view.webContents.loadURL('https://www.google.com');
+
+  ipcMain.on('navigate', (event, url) => {
+    const targetUrl = url.startsWith('http') ? url : `https://${url}`;
+    view.webContents.loadURL(targetUrl);
+  });
+
+  mainWindow.on('resize', () => {
+    const { width, height } = mainWindow.getBounds();
+    view.setBounds({ x: 250, y: 0, width: width - 250, height: height });
+  });
 }
 
 app.whenReady().then(createWindow);
